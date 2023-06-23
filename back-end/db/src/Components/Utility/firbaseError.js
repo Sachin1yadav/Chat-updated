1c719f3697a64985312e14cc7b26c5ab5ca31913
sachin_yadav <sy9084087@gmail.com>
2023-06-23 15:25:45 +0530

    config

--- back-end/db/src/Components/Utility/firbaseError.js
+++
@@ -1,8 +0,0 @@
-const getErrorMessage = (errorCode) => {
-    switch(errorCode){
-        case 'auth/user-not-found':
-            return 'User not found'
-    }
-}
-
-export default getErrorMessage;
